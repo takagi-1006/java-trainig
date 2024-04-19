3f7fd0202c838f9de39a94171eea1b3b8ebccdcd
takagi <ruri-takagi@infinityloop.co.jp>
2024-04-19 14:32:47 +0900

    映画見たい

--- ex-7-2/Main.java
+++ ex-7-2/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("飛行機オブジェクト、ターミナルレーダーオブジェクト、飛行場オブジェクト、進路オブジェクト、天気オブジェクト");
+
+        System.out.println("検索オブジェクト、映画館オブジェクト、上演映画オブジェクト、主演俳優オブジェクト、表示オブジェクト");
+
+        System.out.println("入力オブジェクト、レシピオブジェクト、検索オブジェクト、手順オブジェクト、食材オブジェクト、");
     }
     
 }

052155e9dcb94bf369814620ca4b181cc4fcfe43
takagi <ruri-takagi@infinityloop.co.jp>
2024-04-19 14:25:00 +0900

    ヤッターマン

---
+++ ex-7-1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("配膳ロボット、自動車、電卓、電話対応、スマートフォン、自動ドア、配達、洗濯機、電子レンジ、自動販売機、注文、レジ、カメラ（シャッタータイム）");
+        System.out.println("エレベーター、時計");
+    }
+    
+}

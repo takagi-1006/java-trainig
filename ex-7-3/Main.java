edac97c73c9143a28b9de4b5e4097215a0f3580b
takagi <ruri-takagi@infinityloop.co.jp>
2024-04-19 15:32:47 +0900

    今日は金曜日

---
+++ ex-7-3/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("案内係：");
+        System.out.println("希望条件から観光地検索を表示（操作）、");
+
+        System.out.print("観光地：");
+        System.out.println("案内係で入力された結果を読み取る（属性）、観光名所、住所、所在地、解説を提示（操作）");
+    }
+    
+}
